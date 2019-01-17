35672c2371856fca34ed9f69e839fcf5586f84a5
mkg1 <gehring.mg@gmail.com>
2019-01-16 23:12:19 -0500

    Quicksort problems

---
+++ quicksort.rb
@@ -0,0 +1,10 @@
+# # Chapter 4 - Quicksort
+# 4.1 Write out the code for the earlier sum function
+# 4.2 Write a recursive function to count the number of items in a lists
+# 4.3 Find the maximum number in a list
+# 4.4 Remember binary search? It''s a D&C algo...can you come up with the base case and recursive case for binary search?
+# How long in big O notation?
+# 4.5 Printing the value of each element in an array
+# 4.6 Doubling the value of each element in an array
+# 4.7 Doubling the value of just the first element in an array
+# 4.8 Creating a multiplication table with all the elements in the array. So if array is [2, 3, 7, 8, 10], you first multiply every element by 2, then multiply every element by 3, then by 7, etc.
