aa68ca90e04207dd9d1f0197bdf9219e065c89ee
mkg1 <gehring.mg@gmail.com>
2019-01-17 23:24:01 -0500

    Re-answering binary search written questions

--- binary_search.py
+++ binary_search.py
@@ -1,10 +1,10 @@
 
 #Exercises
 #Chapter 1 - Binary Search
-# 1.1 Sorted list of 128 names; max steps? | 128 -> 7
-# 1.2 Double that list; now how many steps? | 256 -> 8
+# 1.1 Sorted list of 128 names; max steps? | 7 (log(128))
+# 1.2 Double that list; now how many steps? | 8 (log(256))
 # Give the runtime for each scenario:
-# 1.3 Find someone's phone number based on a given name in a phone book. | O(log2n)
+# 1.3 Find someone's phone number based on a given name in a phone book. | O(log(n))
 # 1.4 Find someone's name based on a phone number in a phone book. | O(n)
 # 1.5 Read numbers of every person in the phone book. | O(n)
-# 1.6 Read the numbers of just the A's in the phone book. | O(n + 1)
+# 1.6 Read the numbers of just the A's in the phone book. | O(n) (constants are ignored in bigO notation)
