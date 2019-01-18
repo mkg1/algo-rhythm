4a18e441b942a61f9b6f36ac122b26cfde86f444
mkg1 <gehring.mg@gmail.com>
2019-01-17 23:11:30 -0500

    Add greedy algorithms and selection sort sections

---
+++ greedy_algorithms.rb
@@ -0,0 +1,9 @@
+#Chapter 8 - Greedy Algorithms
+8.1 You work for a furniture co and have to ship all over the country; you need to pack a truck w boxes of diff sizes and trying to maximize space used in each truck. How would you pick boxes to max. space? Come up w greedy strategy. Will that be optimal solution?
+8.2 You're going to Europe and you've got 7 days. You assign a point value to each itinerary list and estimate how long it takes. How can you maximize the point total (see as much as you can that you really want to see)? Use greedy strategy. Optimal Solution?
+8.3 greedy or not? quicksort?
+8.4 greedy or not? BFS?
+8.5 greedy or not? Dj''s algo?
+8.6 Postman needs to deliver to 20 homes; needs to find shortest route. NPC?
+8.7 Finding the largest clique in a set of people...NPC?
+8.8 Making a map of USA and need adjacent states to be diff colors; you have to find minimum number of colors needed. NPC?
