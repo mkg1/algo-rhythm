9693f7799695f42850175d1340fa7622bf125420
mkg1 <gehring.mg@gmail.com>
2019-01-17 23:10:59 -0500

    Add chapter on dynamic programming

---
+++ dynamic_programming.rb
@@ -0,0 +1,6 @@
+#Chapter 9
+9.1 Suppose you steal another item for the knapsack problem - an mp3 player weighing 1lb, worth $1000. Should you steal it?
+9.2 You're going cmaping with a knapsack that'll hold 6lbs and you can take:
+  water/3lbs/10; book/1lb/3; food/2lb/9; jacket/2lb/5; camera/1lb/6
+  what is optimal set to take on camping trip? (each item has weight and rank)
+9.3 Draw and fill in grid to calculate longest common substring between blues and clues
