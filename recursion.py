d631408dfb2fb377841c1432adfdb0e19b84a395
mkg1 <gehring.mg@gmail.com>
2019-01-17 22:57:44 -0500

    Recursion problems

---
+++ recursion.py
@@ -0,0 +1,3 @@
+# Chapter 3 - Recursion
+3.1 What info can you give based on the call stack greet2:name>maggie;greet:name>maggie? |
+3.2 You accidentally write an infinite loop. What happens to the stack when your recursive function runs forever?
