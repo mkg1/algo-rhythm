0792d1a0672c65435d16d0f2b343b29327ca5454
mkg1 <gehring.mg@gmail.com>
2019-01-19 23:38:50 -0500

    Chapter 3 written problems

--- recursion.rb
+++ recursion.rb
@@ -1,3 +1,3 @@
 # Chapter 3 - Recursion
-3.1 What info can you give based on the call stack greet2:name>maggie;greet:name>maggie? |
-3.2 You accidentally write an infinite loop. What happens to the stack when your recursive function runs forever?
+# 3.1 What info can you give based on the call stack greet2:name>maggie;greet:name>maggie? | Greet function called first then calls greet2; greet is in incomplete state; current function call is greet2 - when it resumes, greet function will resume.
+# 3.2 You accidentally write an infinite loop. What happens to the stack when your recursive function runs forever? | The stack continues growing infinitely until the allotted space runs out.
